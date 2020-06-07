ac0e3bc7132ee0d95950869c7dd50f0abd8190f5
AspidPoint <AlexandrFomin88@yandex.ru>
2020-06-08 00:14:16 +0300

    Создал новую ветку, создал 8 объектов, жду завтря для уточнения информации на тему "Что от меня хотят". Может с утра сам пойму..

---
+++ js/main.js
@@ -0,0 +1,196 @@
+"use strict";
+
+var massiveObj = [];
+
+var massiveObj[1] = {
+  author: {
+  avatar: "img/avatars/user1.png"
+},
+  offer: {
+  title: "Заголовок 1"
+  address: x, y,
+  price: 200
+  type: "palace",
+  rooms: 2,
+  guests: 4,
+  checkin: "13:00",
+    checkout: "14:00",
+  features: wifi, dishwasher, parking, washer, elevator, conditioner,
+    description: "строка с описанием",
+    photos: "http://o0.github.io/assets/images/tokyo/hotel1.jpg, http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+},
+  location: {
+  x: 600
+  y: 350.
+}
+};
+
+var  massiveObj[2] = {
+  author: {
+    avatar: "img/avatars/user2.png"
+  },
+  offer: {
+    title: "Заголовок 2",
+    address: x, y,
+    price: 300.
+    type: "flat",
+    rooms: 3,
+    guests: 4,
+    checkin: "13:00",
+    checkout: "14:00",
+    features: wifi, dishwasher, parking, washer, elevator, conditioner,
+    description: "строка с описанием",
+    photos: "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+  },
+  location: {
+    x: 500
+    y: 350.
+  }
+};
+
+var  massiveObj[3] = {
+  author: {
+    avatar: "img/avatars/user3.png"
+  },
+  offer: {
+    title: "Заголовок 3",
+    address: x, y,
+    price: 400.
+    type: "house",
+    rooms: 2,
+    guests: 2,
+    checkin: "12:00",
+    checkout: "13:00",
+    features: wifi, dishwasher, parking, washer, conditioner,
+    description: "строка с описанием",
+    photos: "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+  },
+  location: {
+    x: 650,
+    y: 400.
+  }
+};
+
+var  massiveObj[4] = {
+  author: {
+    avatar: "img/avatars/user4.png"
+  },
+  offer: {
+    title: "Заголовок 4",
+    address: x, y,
+    price: 450.
+    type: "bungalo",
+    rooms: 1,
+    guests: 2,
+    checkin: "13:00",
+    checkout: "14:00",
+    features: wifi, dishwasher, parking, washer,
+    description: "строка с описанием",
+    photos: "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+  },
+  location: {
+    x: 700,
+    y: 450.
+  }
+};
+
+var  massiveObj[5] = {
+  author: {
+    avatar: "img/avatars/user5.png"
+  },
+  offer: {
+    title: "Заголовок 5",
+    address: x, y,
+    price: 750.
+    type: "bungalo",
+    rooms: 1,
+    guests: 2,
+    checkin: "13:00",
+    checkout: "14:00",
+    features: wifi, dishwasher, parking, washer, elevator, conditioner,
+    description: "строка с описанием",
+    photos: "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+  },
+  location: {
+    x: 650,
+    y: 400.
+  }
+};
+
+var  massiveObj[6] = {
+  author: {
+    avatar: "img/avatars/user6.png"
+  },
+  offer: {
+    title: "Заголовок 6",
+    address: x, y,
+    price: 850.
+    type: "palace",
+    rooms: 1,
+    guests: 2,
+    checkin: "13:00",
+    checkout: "14:00",
+    features: wifi, dishwasher, parking, washer, elevator, conditioner,
+    description: "строка с описанием",
+    photos: "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+  },
+  location: {
+    x: 730,
+    y: 700.
+  }
+};
+
+var  massiveObj[7] = {
+  author: {
+    avatar: "img/avatars/user7.png"
+  },
+  offer: {
+    title: "Заголовок 7",
+    address: x, y,
+    price: 850.
+    type: "palace",
+    rooms: 4,
+    guests: 10,
+    checkin: "13:00",
+    checkout: "14:00",
+    features: wifi, dishwasher, parking, washer, elevator, conditioner,
+    description: "строка с описанием",
+    photos: "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+  },
+  location: {
+    x: 710,
+    y: 750.
+  }
+};
+
+var  massiveObj[8] = {
+  author: {
+    avatar: "img/avatars/user8.png"
+  },
+  offer: {
+    title: "Заголовок 8",
+    address: x, y,
+    price: 850.
+    type: "palace",
+    rooms: 4,
+    guests: 10,
+    checkin: "13:00",
+    checkout: "14:00",
+    features: wifi, dishwasher, parking, washer, elevator, conditioner,
+    description: "строка с описанием",
+    photos: "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+  },
+  location: {
+    x: 860,
+    y: 400.
+  }
+};
+
+
+
+function createObject(length) {
+    for (var i = 0; i <= massiveObj.length; i++) {
+      
+      return result;
+    }
+}
