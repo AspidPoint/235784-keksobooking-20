9747db9d23cdc2f89767402c548722320b15a6b1
AspidPoint <AlexandrFomin88@yandex.ru>
2020-06-09 18:36:08 +0300

    Исправленно: 1. Переделаны имена переменных на более читабильные. 2. Созданы дополнительные функции. 3. Отрисовка вынесена из функции генерации.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 
 
 
-/*massiveObj[8] = {
+/*availableRealty[8] = {
   author: {
     avatar: "img/avatars/user8.png"
   },
@@ -23,60 +23,92 @@
   }
 }; */
 
-var massiveObj = [];
-var featuresArr = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-var typeArr = ["palace", "flat", "house", "bungalo"];
-var timeArr = ["12:00", "13:00", "14:00"];
-var photoArr = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
-var findObject = document.querySelector('.map__pins');
+var availableRealty = [];
+var possiblefeatures = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+var realtyTypes = ["palace", "flat", "house", "bungalo"];
+var possibleTimes = ["12:00", "13:00", "14:00"];
+var photosUrl = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+var getWorkArea = document.querySelector('.map__pins');
+var locationArrowX = 25;
+var locationArrowY = 70;
 
 
-function randomInteger(min, max) {
+function getRandomInteger(min, max) {
   var numberRandom = min - 0.5 + Math.random() * (max - min + 1);
   return Math.floor(numberRandom);
 }
 
+function difinitionOfTimes(massive) {
+  return massive[Math.floor(Math.random() * massive.length)];
+}
+
+function getSliceFromFeaures(arrayInto) {
+  var sliceLen = getRandomInteger(0, arrayInto.length);
+  var randomIntos = [];
+  for (var i = 0; i <= sliceLen; i++) {
+    randomIntos.push(difinitionOfTimes(arrayInto));
+  }
+  /*массив на определение уникальности */
+  var uniqRandomIntos = [];
+  for (var i = 0; i < randomIntos.length; i++) {
+    if (!uniqRandomIntos.includes(randomIntos[i])) {
+      uniqRandomIntos.push(randomIntos[i]);
+    }
+  }
+}
+
+getSliceFromFeaures(possiblefeatures);
+
 function createObject(length) {
     for (var i = 1; i <= length; i++) {
-      massiveObj[i] = {
+      availableRealty[i] = {
         author: {
           avatar: "img/avatars/user0" + i + '.png'
         },
         offer: {
           title: "Заголовок " + i,
-          adress: "{{location.x}}, {{location.y}}",
-          price: randomInteger(10000, 50000),
-          type: typeArr[Math.floor(Math.random() * typeArr.length)],
-          rooms: randomInteger(1, 4),
-          guests: randomInteger(1, 3),
-          checkin: timeArr[Math.floor(Math.random() * timeArr.length)],
-          checkout: timeArr[Math.floor(Math.random() * timeArr.length)],
-          features: featuresArr[Math.floor(Math.random() * featuresArr.length)],
+          adress: "",
+          price: getRandomInteger(10000, 50000),
+          type: realtyTypes[Math.floor(Math.random() * realtyTypes.length)],
+          rooms: getRandomInteger(1, 4),
+          guests: getRandomInteger(1, 3),
+          checkin: difinitionOfTimes(possibleTimes),
+          checkout: difinitionOfTimes(possibleTimes),
+          features: getSliceFromFeaures(possiblefeatures),
           description: "строка с описанием",
-          photos: photoArr,
+          photos: getSliceFromFeaures(photosUrl),
         },
         location: {
-          x: randomInteger(100, 800),
-          y: randomInteger(130, 630)
+          x: getRandomInteger(100, 800),
+          y: getRandomInteger(130, 630)
         }
       };
-      var pinButton = document.createElement('button');
-      var pinImage = document.createElement('img');
-
-      pinButton.style = "left:" + massiveObj[i].location.x + "px; " + "top:" + massiveObj[i].location.y + "px";
-      pinButton.classList.add("map__pin");
-      pinButton.type = "button";
-      pinImage.src = massiveObj[i].author.avatar;
-      pinImage.classList.add('standartImg');
-
-      findObject.append(pinButton);
-      pinButton.append(pinImage);
+      /* добавляем сгенерированые координаты в offer.adress */
+      availableRealty[i].offer.adress = String(availableRealty[i].location.x) + "; " + String(availableRealty[i].location.y);
+      availableRealty[i].location.x -=  locationArrowX;
+      availableRealty[i].location.y +=  locationArrowY;
+      
+      drawnRealty(availableRealty[i]);
     }
+
+}
+
+function drawnRealty (realty) {
+  console.log(realty.location);
+
+    var pinButton = document.createElement("button");
+    var pinImage = document.createElement("img");
+
+    pinButton.style = "left:" + realty.location.x + "px; " + "top:" + realty.location.y + "px";
+    pinButton.classList.add("map__pin");
+    pinButton.type = "button";
+    pinImage.src = realty.author.avatar;
+    pinImage.classList.add("standartImg");
+
+    getWorkArea.append(pinButton);
+    pinButton.append(pinImage);
 }
 
 
 
-
-
 createObject(8);
-console.log( massiveObj);
